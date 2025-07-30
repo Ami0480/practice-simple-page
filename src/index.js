2a6cafc35daca1acff70b380e73de39f8556972e
Ami0480 <amifuku80@gmail.com>
2025-07-30 11:24:34 +0800

    Added css and js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+function contactName() {
+  let name = prompt("What's your name?");
 
+  alert(
+    "Hi " + name + ", let's learn about oats. Meanwhile, eat a lot of oats🥣"
+  );
+}
+
+let contactButton = document.querySelector("button");
+
+contactButton.addEventListener("click", contactName);
